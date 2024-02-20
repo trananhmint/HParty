f58d240fd939d37dc79f865f70ca3c8021ad30ae
Minh <trananhmint@gmail.com>
2024-02-21 03:43:56 +0700

    Category(icon) DescriptionBox ServiceDisplay HostServiceInfo

--- src/Components/DescriptionBox/Descriptionbox.jsx
+++ src/Components/DescriptionBox/Descriptionbox.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import './DescriptionBox.css'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const Descriptionbox = () => {
   return (
-    <div className='descriptionbox'>
+    <div className="descriptionbox">
+      <div className='descriptionbox-description'>
+        <h2>Service Details</h2>
+        <hr />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias fugit fugiat consectetur!
+          Consequatur soluta quisquam quo dicta nesciunt! Nobis quaerat inventore fugiat laborum porro
+          voluptates fugit nam magni, eum maxime. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Officiis eaque dolorem perferendis eius non quasi laborum impedit at neque ad nostrum, numquam illum.
+          Cum, repellendus incidunt voluptatum voluptatibus fugiat accusamus! Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Praesentium distinctio fuga cum facere molestias natus modi quis!
+          Ullam delectus, maxime neque voluptates tenetur qui, adipisci earum architecto, dolorum tempora aspernatur.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero facilis dignissimos minima officia molestias
+          iure optio eligendi atque modi quam natus, quod earum tenetur labore? Minima voluptatum magni harum quisquam.
+        </p>
 
+      </div>
+      {/* <hr /> */}
+      <div className="descriptionbox-facilities">
+        <h2>Facilities</h2>
+        <hr />
+        <div className="facilities">
+          <p><CheckCircleIcon/> Wifi free</p>
+          <p><CheckCircleIcon/> Non-smoking rooms</p>
+          <p><CheckCircleIcon/> Bar</p>
+          <p><CheckCircleIcon/> Airport shuttle</p>
+          <p><CheckCircleIcon/> Beach front</p>
+        </div>
+      </div>
     </div>
+
   )
 }
 
--- src/Components/ServiceDisplay/ServiceDisplay.jsx
+++ src/Components/ServiceDisplay/ServiceDisplay.jsx
@@ -4,10 +4,16 @@ import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
 import { ServiceContext } from '../../Context/ServiceContext';
 
+import Rating from '@mui/material/Rating';
+
 
 const ServiceDisplay = (props) => {
   const { service } = props;
+
   const { addToCart } = useContext(ServiceContext);
+
+  const [value, setValue] = React.useState(2);
+
   return (
     <div className='servicedisplay'>
       <div className="servicedisplay-left">
@@ -22,24 +28,34 @@ const ServiceDisplay = (props) => {
         </div>
       </div>
       <div className="servicedisplay-right">
-        <h1>{service.serviceName}</h1>
+        <h1>{service.serviceName} Party room 200m2</h1>
         <div className="servicedisplay-right-star">
+          {/* <StarOutlinedIcon />
           <StarOutlinedIcon />
           <StarOutlinedIcon />
           <StarOutlinedIcon />
-          <StarOutlinedIcon />
-          <StarBorderOutlinedIcon />
-          <p>({service.sale}100)</p>
+          <StarBorderOutlinedIcon /> */}
+            <Rating
+              name="simple-controlled"
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+            />
+
+            <p>({service.sale}100)</p>
         </div>
         <div className="servicedisplay-right-prices">
-          <div className="servicedisplay-right-price">{service.price} đ</div>
-          <div className="servicedisplay-right-price-sale">{service.price} đ</div>
+          <div className="servicedisplay-right-price">{service.price}800.000 đ</div>
+          <div className="servicedisplay-right-price-sale">{service.price}500.000 đ</div>
         </div>
         <div className="servicedisplay-right-description">
-          <p>{service.description}</p>
+          <p>{service.description}
+            🌿🌿🌿THẢM TRANG TRÍ PHÒNG KHÁCH PHAM STYLE🌿🌿🌿
+          </p>
         </div>
-        <button onClick={()=>{addToCart(service.id)}}>ADD TO CART</button>
-        
+        <button onClick={() => { addToCart(service.id) }}>ADD TO CART</button>
+
       </div>
 
     </div>

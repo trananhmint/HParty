9ea5bc0b5a9c1fb65e6cb840ade94297d02aca5c
Minh <trananhmint@gmail.com>
2024-02-26 00:43:02 +0700

    serviceContext

--- src/Context/ServiceContext.jsx
+++ src/Context/ServiceContext.jsx
@@ -1,13 +1,11 @@
 import React, { createContext, useEffect, useState } from "react";
-import { useDispatch} from 'react-redux';
 import axios from "axios";
 import { addToCart } from "../redux/cartSlice";
 
-import all_service from '../Components/Assets/all_service'
 import { FetchProduct } from "./fetchData";
-import { UseDispatch, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from "axios";
-import { addToCart } from "../redux/CartSlice";
+// import { addToCart } from "../redux/CartSlice";
 
 export const ServiceContext = createContext(null);
 
@@ -46,31 +44,9 @@ const ServiceContextProvider = (props) => {
     }, []);
 
 
-    useEffect(() => {
-        async function fetchData() {
-            let response = await axios.get('https://bookingbirthdayparties.azurewebsites.net/api/Service/services')
-            setServices(response.data.data);
-    const [product, setProduct] = useState(GetDefaultCart());
-    const [cartItems, setCartItems] = useState(GetDefaultCart());
-    const [totalPrice, setTotalPrice] = useState(0);
-
-
-    const [rooms, setRooms] = useState([]);
-    const [services, setServices] = useState([]);
-
-    const dispatch = useDispatch();
 
 
 
-
-    useEffect(() => {
-        async function fetchData() {
-            let response = await axios.get('https://bookingbirthdayparties.azurewebsites.net/api/Room')
-            setRooms(response.data.data);
-            console.log(response.data);
-        }
-        fetchData();
-    }, []);
 
 
     useEffect(() => {
@@ -92,20 +68,6 @@ const ServiceContextProvider = (props) => {
         console.log(product);
     }
 
-    const AddToCart = (itemId) => {
-        fetchData();
-    }, []);
-
-    dispatch(addToCart({ rooms, services, totalPrice }));
-
-    // const cart = useSelector((state) => state.cart.cart)
-    // console.log(cart);
-
-    const AddRoomsToCart = (itemId) => {
-        setProduct((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
-        console.log("Add to cart");
-        console.log(product);
-    }
 
     const AddToCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
@@ -114,15 +76,6 @@ const ServiceContextProvider = (props) => {
 
     }
 
-    const removeRoomsFromCart = (itemId) => {
-        if (product[itemId] > 0) {
-            setProduct((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
-        }
-
-        console.log("Add to cart");
-        console.log(cartItems);
-
-    }
 
     const removeRoomsFromCart = (itemId) => {
         if (product[itemId] > 0) {
@@ -151,20 +104,9 @@ const ServiceContextProvider = (props) => {
                 }
             }
         }
-        let sumPrice = 0;
 
-        for (let key in product) {
-            if (product[key] > 0) {
-                let roomInfo = rooms.find((room) => Number(room.roomId) === Number(key))
-                console.log(roomInfo)
-                if (roomInfo) {
-                    sumPrice += roomInfo.price * product[key];
-                }
-            }
-        }
         for (let key in cartItems) {
             if (cartItems[key] > 0) {
-                let serviceInfo = services.find((service) => Number(service.serviceId) === Number(key))
                 let serviceInfo = services.find((service) => Number(service.serviceId) === Number(key))
                 console.log(serviceInfo)
                 if (serviceInfo) {
@@ -172,8 +114,6 @@ const ServiceContextProvider = (props) => {
                 }
             }
         }
-        setTotalPrice(totalPrice + sumPrice);
-        return totalPrice + sumPrice;
         setTotalPrice(totalPrice + sumPrice);
         return totalPrice + sumPrice;
     }
@@ -187,12 +127,7 @@ const ServiceContextProvider = (props) => {
             }
         }
 
-        let roomCount = 0;
-        for (let index in product) {
-            if (product[index] > 0) {
-                roomCount += product[index];
-            }
-        }
+
 
         for (let index in cartItems) {
             if (cartItems[index] > 0) {
@@ -200,11 +135,9 @@ const ServiceContextProvider = (props) => {
             }
         }
         return count + roomCount;
-        return count + roomCount;
     }
 
 
-    const contextValue = { services, rooms, cartItems, product, AddToCart, AddRoomsToCart, removeFromCart, removeRoomsFromCart, getTotalPrice, getCountOfCart, totalPrice };
     const contextValue = { services, rooms, cartItems, product, AddToCart, AddRoomsToCart, removeFromCart, removeRoomsFromCart, getTotalPrice, getCountOfCart, totalPrice  };
     return <ServiceContext.Provider value={contextValue}>
         {props.children}
